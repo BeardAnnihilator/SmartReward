7739aaeb56444cb338b15c41fdc5ba766e25fc67
Alexandre <periphanos.a@gmail.com>
2014-08-29 13:55:01 +0200

    New: dynamic search

---
+++ SmartReward/SmartReward/Scripts/SmartReward/Binding.js
@@ -0,0 +1,26 @@
+﻿
+$(function () {
+
+    var ajaxSubmitForm = function () {
+        var $form = $(this);
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done(function (data) {
+            var $target = $("#searchResult");
+            $target.replaceWith(data);
+        });
+
+        return false;
+    }
+
+    $('#searchTerm').submit(ajaxSubmitForm);
+
+    $('#searchTerm').bind('input', function () {
+        $("#searchForm").submit();
+    });
+});
+
