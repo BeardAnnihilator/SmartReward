129969d5d7a853ddcfe092b0a5b027f53b34e9d0
Alexandre <periphanos.a@gmail.com>
2014-09-29 17:59:44 +0200

    New: notifications signalR exemple chat
    NEXT STEP :
    http://www.asp.net/signalr/overview/signalr-20/hubs-api/mapping-users-to-connections

--- SmartReward/SmartReward/Controllers/HomeController.cs
+++ SmartReward/SmartReward/Controllers/HomeController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SmartReward.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             if (User.Identity.IsAuthenticated)
